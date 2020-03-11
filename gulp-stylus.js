e402b9a795dbc8adf4ef4bf4a417b848187f0c08
mar <mar.nospam@anomy.net>
2020-03-11 13:29:11 +0000

    style: Clean up unused/invalid cssnano rules

--- gulp-stylus.js
+++ gulp-stylus.js
@@ -34,14 +34,7 @@ module.exports = (opts) => {
         postcss([
           autoprefixer(),
           cssnano({
-            preset: [
-              'default',
-              {
-                discardUnused: true,
-                mergeIdents: true,
-                cssDeclarationSorter: { keepOverrides: true },
-              },
-            ],
+            preset: ['default', { cssDeclarationSorter: { keepOverrides: true } }],
           }),
         ])
       )
